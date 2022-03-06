e0ee39b06bfca9e20b47afca437c24f38fdf8646
freedomusertech <38665902+freedomusertech@users.noreply.github.com>
2022-03-06 11:57:36 +0100

    Update mekkah.py

--- scripts/mekkah.py
+++ scripts/mekkah.py
@@ -18,7 +18,7 @@ def grab(url):
         os.system(f'wget {url} -O temp.txt')
         response = ''.join(open('temp.txt').readlines())
         if '.m3u8' not in response:
-            print('ttps://raw.githubusercontent.com/freedomusertech/1st/main/assets/info.m3u8')
+            print('https://raw.githubusercontent.com/freedomusertech/1st/main/assets/info.m3u8')
             return
     end = response.find('.m3u8') + 5
     tuner = 100
